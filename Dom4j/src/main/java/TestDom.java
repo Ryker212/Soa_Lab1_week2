2ce9938df3889c05df27bcab950b0068394f7b87
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-10 15:22:52 +0700

    สร้างtestdom แบบดึง simple
    สร้างtestdom แบบดึงไฟล์simple.xml

--- Dom4j/src/main/java/TestDom.java
+++ Dom4j/src/main/java/TestDom.java
@@ -1,29 +1,28 @@
-import java.io.FileOutputStream;
+import java.io.File;
+import java.util.Iterator;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentFactory;
 import org.dom4j.Element;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.XMLWriter;
+import org.dom4j.io.SAXReader;
 
 public class TestDom {
 	
 	public static void main(String[] args)throws Exception {
 		
-		Document doc = DocumentFactory.
-				getInstance().createDocument();
-		Element root = doc.addElement("root");
-		root.addAttribute("myAttribute", "myValue");
-		Element elem = root.addElement("myelem");
-		elem.addText("Hello World");
+		File xml = new File("simple.xml");
+		SAXReader reader = new SAXReader();
+		Document doc = reader.read(xml);
+		Element root = doc.getRootElement();
 		
-		FileOutputStream fos = new FileOutputStream("simple.xml");
-		OutputFormat format = OutputFormat.createPrettyPrint();
-		XMLWriter writer = new XMLWriter(fos, format);
-		writer.write(doc);
-		writer.flush();
-		writer.close();
-		fos.close();
+		Iterator i = root.elements().iterator();
+		
+		while(i.hasNext()) {
+			
+			Element e = (Element)i.next();
+			System.out.println(e.getPath());
+			System.out.println(e.getName());
+			System.out.println(e.getData());
+		}
 	}
 				
 	
