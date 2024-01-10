405a65197c80bd5087fbba8049140fc32ea86858
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-10 15:35:52 +0700

    ทำtestdom pull student
    ทำtestdom pull student
    -เปลี่ยนไฟล์simple.xmlให้มีstudent
    -เพิ่มdependency library jaxen ไฟล์pom

--- Dom4j/src/main/java/TestDom.java
+++ Dom4j/src/main/java/TestDom.java
@@ -1,30 +1,34 @@
-import java.io.FileOutputStream;
+import java.io.File;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentFactory;
+import org.dom4j.DocumentException;
 import org.dom4j.Element;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.XMLWriter;
+import org.dom4j.io.SAXReader;
+
 
 public class TestDom {
-	
-	public static void main(String[] args)throws Exception {
-		
-		Document doc = DocumentFactory.
-				getInstance().createDocument();
-		Element root = doc.addElement("root");
-		root.addAttribute("myAttribute", "myValue");
-		Element elem = root.addElement("myelem");
-		elem.addText("Hello World");
-		
-		FileOutputStream fos = new FileOutputStream("simple.xml");
-		OutputFormat format = OutputFormat.createPrettyPrint();
-		XMLWriter writer = new XMLWriter(fos, format);
-		writer.write(doc);
-		writer.flush();
-		writer.close();
-		fos.close();
+
+	public static void main(String[] args) {
+		try {
+			File inputFile = new File("simple.xml");
+			SAXReader reader = new SAXReader();
+			Document document = reader.read(inputFile);
+			System.out.println("Root element :" + document.getRootElement().getName());
+			Element classElement = document.getRootElement();
+			java.util.List<org.dom4j.Node> nodes = document.selectNodes("/class/student[@rollno = '493']");
+			System.out.println("----------------------------");
+
+			for (org.dom4j.Node node : nodes) {
+				System.out.println("\nCurrent Element :" + node.getName());
+				System.out.println("Student roll no : " + node.valueOf("@rollno"));
+				System.out.println("First Name : " + node.selectSingleNode("firstname").getText());
+				System.out.println("Last Name : " + node.selectSingleNode("lastname").getText());
+				System.out.println("First Name : " + node.selectSingleNode("nickname").getText());
+				System.out.println("Marks : " + node.selectSingleNode("marks").getText());
+			}
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
 	}
-				
-	
+
 }
